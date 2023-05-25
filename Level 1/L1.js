2b77cebc9773606174f5b0ca44e6732f28471dd5
Astro13245 <121739164+Astro13245@users.noreply.github.com>
2023-05-25 19:52:08 -0400

    Put music on HTML file

--- Level 1/L1.js
+++ Level 1/L1.js
@@ -1,10 +1,5 @@
 import { movePerson, checkIfPositionIsRight } from '../Config/Sort';
 
-const music = new Audio('/Music/track_1.wav');
-music.play();
-music.loop = true;
-music.playbackRate = 1;
-
 let people = [
     { name: "Person1", position: 2, endingPosition: 1, description: "The Willow Project will release 287 million metric tons of CO2 over 30 years", ifClicked: false },  
     { name: "Person2", position: 4, endingPosition: 2, description: "The Willow Project will release 287 million metric tons of CO2 over 20 years", ifClicked: false },  
